b728cf3299306ada5bf6c718ef50304a22a26496
vsparrow <khan@vsparrow.com>
2018-03-12 14:09:21 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,12 @@
 class Application < Sinatra::Base
   # Write your code here!
+  get '/' do
+    erb :index
+  end
 
+  post '/greet' do
+    @params = params
+    # resp.write @params
+    erb :greet
+  end
 end
